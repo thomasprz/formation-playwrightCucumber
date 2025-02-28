8fc7bcdb41073db35e16f595230c1a1f6f76ce40
Thomas <przegralek.thomas@gmail.com>
2025-02-28 17:11:22 +0100

    Ajout d'un test lié à la connexion avec différents utilisateurs

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -1,18 +1,26 @@
-const {Given, When, Then} = require('@cucumber/cucumber')
+const {Given, When, Then, defineStep} = require('@cucumber/cucumber')
 const {LoginPage} = require ('../pages-objects/login-page')
 const {InventoryPage} = require ('../pages-objects/inventory-page')
 
 const loginPage = new LoginPage()
 const inventoryPage = new InventoryPage()
 
-Given("I visit a login page", async function () {
+defineStep("I visit a login page", async function () {
     await loginPage.navigateToLoginScreen()
 })
 
-When("I fill the login form with valid credentials", async function () {
+defineStep("I fill the login form with valid credentials", async function () {
     await loginPage.submitLoginForm()
 })
 
-Then("I should see the home page", async function() {
-   await inventoryPage.assertUserIsLoggedIn()
+defineStep("I should see the home page", async function() {
+    await inventoryPage.assertUserIsLoggedIn()
+})
+
+defineStep("I wait for 3 seconds", async function () {
+    await loginPage.pause()
+})
+
+defineStep(/I fill the login form with "([^"]*)" and "([^"]*)"/, async function (username, password) {
+    await loginPage.submitLoginFormWithParameters(username, password);
 })
