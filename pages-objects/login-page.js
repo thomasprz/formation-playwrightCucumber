d0b7af3f2a81d7eb42f517854cd3b0765de0d050
Thomas <przegralek.thomas@gmail.com>
2025-02-28 17:10:10 +0100

    Mise à jour de la page lié au Login

--- pages-objects/login-page.js
+++ pages-objects/login-page.js
@@ -8,4 +8,14 @@ export class LoginPage{
         await page.fill('#password', 'secret_sauce')
         await page.click('#login-button')
     }
+
+    async submitLoginFormWithParameters(username, password){
+        await page.fill('#user-name', username)
+        await page.fill('#password', password)
+        await page.click('#login-button')
+    }
+
+    async pause(){
+        await page.waitForTimeout(3000)
+    }
 }
